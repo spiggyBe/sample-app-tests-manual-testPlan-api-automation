56115020df6a84f322ef0b614077682dde185e2c
Zbigniew Bernacki <38226389+spiggyBe@users.noreply.github.com>
2025-02-18 09:11:27 +0100

    Delete fixtures directory

--- fixtures/urlAndAuthorizationData.ts
+++
@@ -1,8 +0,0 @@
-import { UrlAndAuthorizationData } from '../types/types'
-
-export const urlAndAuthorizationData: UrlAndAuthorizationData = {
-  authorizationToken: 'Bearer 123 - can also be hidden in .ENV depends of project and data sensitivity',
-  createLike: 'https://api.SAMPLE-APP.com/api/web/observations/501/like',
-  deleteLike: 'https://api.SAMPLE-APP.com/api/web/observations/501/like',
-  getSingleObservation: 'https://api.SAMPLE-APP.com/api/web/observations/501'
-}
